ebb32970cb4e64acf768bb03018efb25d8312915
Marc Mengel <mengel@fnal.gov>
2021-01-11 13:05:57 -0600

    os scope patches

--- lib/spack/spack/config.py
+++ lib/spack/spack/config.py
@@ -746,6 +746,14 @@ def _add_platform_scope(cfg, scope_type, name, path):
     cfg.push_scope(scope_type(plat_name, plat_path))
 
 
+def _add_os_scope(cfg, scope_type, name, path):
+    """Add an os-specific subdirectory for the current platform."""
+    oss = spack.architecture.sys_type().split('-')[1]
+    os_name = '%s/%s' % (name, oss)
+    os_path = '%s/%s' % (path, oss)
+    cfg.push_scope(scope_type(os_name, os_path))
+
+
 def _add_command_line_scopes(cfg, command_line_scopes):
     """Add additional scopes from the --config-scope argument.
 
@@ -763,6 +771,7 @@ def _add_command_line_scopes(cfg, command_line_scopes):
         name = 'cmd_scope_%d' % i
         cfg.push_scope(ImmutableConfigScope(name, path))
         _add_platform_scope(cfg, ImmutableConfigScope, name, path)
+        _add_os_scope(cfg, ImmutableConfigScope, name, path)
 
 
 def _config():
@@ -788,6 +797,7 @@ def _config():
 
         # Each scope can have per-platfom overrides in subdirectories
         _add_platform_scope(cfg, ConfigScope, name, path)
+        _add_os_scope(cfg, ConfigScope, name, path)
 
     # add command-line scopes
     _add_command_line_scopes(cfg, command_line_scopes)
