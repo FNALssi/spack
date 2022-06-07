d96f117163119c7e9ed8bccfa9a47b061d9fbd24
Marc Mengel <mengel@fnal.gov>
2022-06-07 14:20:16 -0500

    fix _add_os_scope

--- lib/spack/spack/config.py
+++ lib/spack/spack/config.py
@@ -762,7 +762,8 @@ def _add_platform_scope(cfg, scope_type, name, path):
 
 def _add_os_scope(cfg, scope_type, name, path):
     """Add an os-specific subdirectory for the current platform."""
-    oss = spack.architecture.sys_type().split('-')[1]
+    host_platform = spack.platforms.host()
+    oss = host_platform.operating_system('frontend')
     os_name = '%s/%s' % (name, oss)
     os_path = '%s/%s' % (path, oss)
     cfg.push_scope(scope_type(os_name, os_path))
@@ -839,6 +840,7 @@ def _config():
 
         # Each scope can have per-platfom overrides in subdirectories
         _add_platform_scope(cfg, ConfigScope, name, path)
+        _add_os_scope(cfg, ConfigScope, name, path)
 
     # add command-line scopes
     _add_command_line_scopes(cfg, command_line_scopes)
