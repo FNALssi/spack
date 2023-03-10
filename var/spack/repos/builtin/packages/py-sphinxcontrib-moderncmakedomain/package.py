4106d614b3005d9f9a3ef8c50620d06c94af5faf
Chris Green <greenc@fnal.gov>
2023-03-10 15:08:34 -0600

    [py-sphinxcontrib-moderncmakedomain] Whitespace fix

--- var/spack/repos/builtin/packages/py-sphinxcontrib-moderncmakedomain/package.py
+++ var/spack/repos/builtin/packages/py-sphinxcontrib-moderncmakedomain/package.py
@@ -34,7 +34,7 @@ class PySphinxcontribModerncmakedomain(PythonPackage):
     version("3.25.0", sha256="4138e4d3f60e5c4b3982caa10033693bfc1009cdd851766754d5990d9d1e992a")
 
     conflicts("python@:3.5")
-    
+
     depends_on("py-hatchling", type="build")
 
     depends_on("py-sphinx", type=("build", "run"))
