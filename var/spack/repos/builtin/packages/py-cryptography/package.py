3e6f23cadc944788846b1d2b98e94dfff163cb43
Chris Green <greenc@fnal.gov>
2023-03-10 09:46:32 -0600

    [py-cryptography] Version updates with better openssl version handling

--- var/spack/repos/builtin/packages/py-cryptography/package.py
+++ var/spack/repos/builtin/packages/py-cryptography/package.py
@@ -13,12 +13,15 @@ class PyCryptography(PythonPackage):
     homepage = "https://github.com/pyca/cryptography"
     pypi = "cryptography/cryptography-1.8.1.tar.gz"
 
+    version("39.0.2", sha256="bc5b871e977c8ee5a1bbc42fa8d19bcc08baf0c51cbf1586b0e87a2694dde42f")
+    version("38.0.4", sha256="175c1a818b87c9ac80bb7377f5520b7f31b3ef2a0004e2420319beadedb67290")
     version("38.0.1", sha256="1db3d807a14931fa317f96435695d9ec386be7b84b618cc61cfa5d08b0ae33d7")
     version("37.0.4", sha256="63f9c17c0e2474ccbebc9302ce2f07b55b3b3fcb211ded18a42d5764f5c10a82")
     version("36.0.1", sha256="53e5c1dc3d7a953de055d77bef2ff607ceef7a2aac0353b5d630ab67f7423638")
     version("35.0.0", sha256="9933f28f70d0517686bd7de36166dda42094eac49415459d9bdf5e7df3e0086d")
     version("3.4.8", sha256="94cc5ed4ceaefcbe5bf38c8fba6a21fc1d365bb8fb826ea1688e3370b2e24a1c")
     version("3.4.7", sha256="3d10de8116d25649631977cb37da6cbdd2d6fa0e0281d014a5b7d337255ca713")
+    version("3.3.2", sha256="5a60d3780149e13b7a6ff7ad6526b38846354d11a15e21068e57073e29e19bed")
     version("3.2.1", sha256="d3d5e10be0cf2a12214ddee45c6bd203dab435e3d83b4560c03066eda600bfe3")
     version("2.7", sha256="e6347742ac8f35ded4a46ff835c60e68c22a536a8ae5c4422966d06946b6d4c6")
     version("2.3.1", sha256="8d10113ca826a4c29d5b85b2c4e045ffa8bad74fb525ee0eceb1d38d4c70dfd6")
@@ -48,7 +51,14 @@ class PyCryptography(PythonPackage):
     depends_on("py-six@1.4.1:", type=("build", "run"), when="@:3.3")
     depends_on("py-idna@2.1:", type=("build", "run"), when="@:2.4")  # deprecated
     depends_on("py-idna@2.1:", type=("build", "run"), when="@2.5: +idna")  # deprecated
-    depends_on("openssl@:1.0", when="@:1.8.1")
+    depends_on("openssl@:1.0", when="@:1.4")
+    depends_on("openssl@1.0.0:1.1.0", when="@1.5:1.6")
+    depends_on("openssl@1.0.1:1.1.0", when="@1.7:2.3")
+    depends_on("openssl@1.0.1:1.1.1", when="@2.4:2.9")
+    depends_on("openssl@1.0.2:1.1.1", when="@3.0:3.2")
+    depends_on("openssl@1.1.0:1.1.1", when="@3.3:")
+    depends_on("openssl@1.1.0:", when="@35:38")
+    depends_on("openssl@1.1.1:", when="@39:")
     depends_on("openssl")
 
     # To fix https://github.com/spack/spack/issues/29669
