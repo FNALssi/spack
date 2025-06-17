bcc41ae5755b2530afef1482b64fb830b1744a72
Patrick Gartung <gartung@fnal.gov>
2025-06-17 12:18:38 -0500

    comment out code using spack_repo

--- lib/spack/spack/build_environment.py
+++ lib/spack/spack/build_environment.py
@@ -1066,10 +1066,10 @@ class SetupContext:
             if os.path.isdir(bin_dir):
                 env.prepend_path("PATH", bin_dir)
 
-        spack.repo.PATH.repos
-        for cp_dir in spack_repo.builtin.build_systems.cmake.get_cmake_prefix_path(dep.package):
-            env.append_path("CMAKE_PREFIX_PATH", cp_dir)
-        env.prune_duplicate_paths("CMAKE_PREFIX_PATH")
+#        spack.repo.PATH.repos
+#        for cp_dir in spack_repo.builtin.build_systems.cmake.get_cmake_prefix_path(dep.package):
+#            env.append_path("CMAKE_PREFIX_PATH", cp_dir)
+#        env.prune_duplicate_paths("CMAKE_PREFIX_PATH")
 
 
 def load_external_modules(context: SetupContext) -> None:
